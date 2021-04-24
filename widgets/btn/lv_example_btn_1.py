5cf6fdf267af51269255b19ddd9f0b521761ee4f
Uli Raich <uli.raich@gmail.com>
2021-04-24 16:32:54 +0200

    added source to event callback to distinguish between the 2 buttons

--- widgets/btn/lv_example_btn_1.py
+++ widgets/btn/lv_example_btn_1.py
@@ -3,19 +3,24 @@ import time
 import lvgl as lv
 import display_driver
 
-def event_handler(evt):
+def event_handler(evt,source):
     code = evt.get_code()
-    print(code)
+
     if code == lv.EVENT.CLICKED:
-        print("Clicked")
-    elif code == lv.EVENT.VALUE_CHANGED:
-        print("Toggled")
-            
+        if source == btn1:
+            print("btn1 Clicked")
+        else:
+            print("btn2 Toggled")
+        
+log_level=["Trace", "Info", "Warning", "Error", "User"]
+lv.log_register_print_cb(lambda level,filename,line,func,msg: print('LOG: %s, file: %s in %s, line %d: %s' % (log_level[level], filename, func, line, msg)))           
+
+#lv.log_add("User","Hello")
 # create a simple button
 btn1 = lv.btn(lv.scr_act())
 
 # attach the callback
-btn1.add_event_cb(event_handler,lv.EVENT.CLICKED, None)
+btn1.add_event_cb(lambda evt: event_handler(evt,btn1),lv.EVENT.CLICKED, None)
 
 btn1.align(lv.ALIGN.CENTER,0,-40)
 label=lv.label(btn1)
@@ -25,7 +30,8 @@ label.set_text("Button")
 btn2 = lv.btn(lv.scr_act())
 
 # attach the callback
-btn2.add_event_cb(event_handler,lv.EVENT.VALUE_CHANGED,None)
+#btn2.add_event_cb(event_handler,lv.EVENT.VALUE_CHANGED,None)
+btn2.add_event_cb(lambda evt: event_handler(evt,btn2),lv.EVENT.CLICKED, None)
 
 btn2.align(lv.ALIGN.CENTER,0,40)
 btn2.add_flag(lv.obj.FLAG.CHECKABLE)
