f3fc259f7753c0e6639976098ab1d0152a294656
Uli Raich <uli.raich@gmail.com>
2021-04-24 16:36:57 +0200

    allow all event in btn example

--- widgets/btn/lv_example_btn_1.py
+++ widgets/btn/lv_example_btn_1.py
@@ -11,6 +11,8 @@ def event_handler(evt,source):
             print("btn1 Clicked")
         else:
             print("btn2 Toggled")
+    elif code == lv.EVENT.VALUE_CHANGED:
+        print("Value changed seen")
         
 log_level=["Trace", "Info", "Warning", "Error", "User"]
 lv.log_register_print_cb(lambda level,filename,line,func,msg: print('LOG: %s, file: %s in %s, line %d: %s' % (log_level[level], filename, func, line, msg)))           
@@ -20,7 +22,7 @@ lv.log_register_print_cb(lambda level,filename,line,func,msg: print('LOG: %s, fi
 btn1 = lv.btn(lv.scr_act())
 
 # attach the callback
-btn1.add_event_cb(lambda evt: event_handler(evt,btn1),lv.EVENT.CLICKED, None)
+btn1.add_event_cb(lambda evt: event_handler(evt,btn1),lv.EVENT.ALL, None)
 
 btn1.align(lv.ALIGN.CENTER,0,-40)
 label=lv.label(btn1)
@@ -31,7 +33,7 @@ btn2 = lv.btn(lv.scr_act())
 
 # attach the callback
 #btn2.add_event_cb(event_handler,lv.EVENT.VALUE_CHANGED,None)
-btn2.add_event_cb(lambda evt: event_handler(evt,btn2),lv.EVENT.CLICKED, None)
+btn2.add_event_cb(lambda evt: event_handler(evt,btn2),lv.EVENT.ALL, None)
 
 btn2.align(lv.ALIGN.CENTER,0,40)
 btn2.add_flag(lv.obj.FLAG.CHECKABLE)
