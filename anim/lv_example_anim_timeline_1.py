9fdd734003bcefe49cc08916e94b49137d1139b7
Uli Raich <uli.raich@gmail.com>
2021-07-27 17:17:18 +0200

    added lv_example_anim_3

--- anim/lv_example_anim_timeline_1.py
+++ anim/lv_example_anim_timeline_1.py
@@ -1,127 +1,148 @@
-#!/opt/bin/lv_mp -i
+#!/opt/bin/lv_micropython -i
 import time
 import lvgl as lv
 import display_driver
 
-obj_width = 120
-obj_height = 200
+class LV_ExampleAnimTimeline_1(object):
 
-def set_width(obj, v):
-    # obj = lv.obj.__cast__(a.user_data)
-    obj.set_width(v)
+    def __init__(self):
+        self.obj_width = 120
+        self.obj_height = 150
+        #
+        # Create an animation timeline
+        #
 
+        self.par = lv.scr_act()
+        self.par.set_flex_flow(lv.FLEX_FLOW.ROW)
+        self.par.set_flex_align(lv.FLEX_ALIGN.SPACE_AROUND, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER)
+        
+        self.btn_run = lv.btn(self.par)
+        self.btn_run.add_event_cb(self.btn_run_event_handler, lv.EVENT.VALUE_CHANGED, None)
+        self.btn_run.add_flag(lv.obj.FLAG.IGNORE_LAYOUT)
+        self.btn_run.add_flag(lv.obj.FLAG.CHECKABLE)
+        self.btn_run.align(lv.ALIGN.TOP_MID, -50, 20)
+        
+        self.label_run = lv.label(self.btn_run)
+        self.label_run.set_text("Run")
+        self.label_run.center()
+        
+        self.btn_del = lv.btn(self.par)
+        self.btn_del.add_event_cb(self.btn_del_event_handler, lv.EVENT.CLICKED, None)
+        self.btn_del.add_flag(lv.obj.FLAG.IGNORE_LAYOUT)
+        self.btn_del.align(lv.ALIGN.TOP_MID, 50, 20)
+        
+        self.label_del = lv.label(self.btn_del)
+        self.label_del.set_text("Stop")
+        self.label_del.center()
+        
+        self.slider = lv.slider(self.par)
+        self.slider.add_event_cb(self.slider_prg_event_handler, lv.EVENT.VALUE_CHANGED, None)
+        self.slider.add_flag(lv.obj.FLAG.IGNORE_LAYOUT)
+        self.slider.align(lv.ALIGN.BOTTOM_RIGHT, -20, -20)
+        self.slider.set_range(0, 65535)
+        
+        self.obj1 = lv.obj(self.par)
+        self.obj1.set_size(self.obj_width, self.obj_height)
+        
+        self.obj2 = lv.obj(self.par)
+        self.obj2.set_size(self.obj_width, self.obj_height)
+        
+        self.obj3 = lv.obj(self.par)
+        self.obj3.set_size(self.obj_width, self.obj_height)
+        
+        self.anim_timeline = None
 
-def set_height(obj, v):
-    # obj = lv.obj.__cast__(a.user_data)
-    obj.set_height(v)
+    def set_width(self,obj, v):
+        obj.set_width(v)
 
+    def set_height(self,obj, v):
+        obj.set_height(v)
 
-def event_handler(e):
-    obj = e.get_target()
+    def anim_timeline_create(self):
+        # obj1 
+        self.a1 = lv.anim_t()
+        self.a1.init()
+        self.a1.set_values(0, self.obj_width)
+        self.a1.set_early_apply(False)
+        self.a1.set_custom_exec_cb(lambda a,v: self.set_width(self.obj1,v))
+        self.a1.set_path_cb(lv.anim_t.path_overshoot)
+        self.a1.set_time(300)
+        
+        self.a2 = lv.anim_t()
+        self.a2.init()
+        self.a2.set_values(0, self.obj_height)
+        self.a2.set_early_apply(False)
+        self.a2.set_custom_exec_cb(lambda a,v: self.set_height(self.obj1,v))
+        self.a2.set_path_cb(lv.anim_t.path_ease_out)
+        self.a2.set_time(300)
+        
+        # obj2 
+        self.a3=lv.anim_t()
+        self.a3.init()
+        self.a3.set_values(0, self.obj_width)
+        self.a3.set_early_apply(False)
+        self.a3.set_custom_exec_cb(lambda a,v: self.set_width(self.obj2,v))
+        self.a3.set_path_cb(lv.anim_t.path_overshoot)
+        self.a3.set_time(300)
+        
+        self.a4 = lv.anim_t()
+        self.a4.init()
+        self.a4.set_values(0, self.obj_height)
+        self.a4.set_early_apply(False)
+        self.a4.set_custom_exec_cb(lambda a,v: self.set_height(self.obj2,v))
+        self.a4.set_path_cb(lv.anim_t.path_ease_out)
+        self.a4.set_time(300)
+        
+        # obj3 
+        self.a5 = lv.anim_t()
+        self.a5.init()
+        self.a5.set_values(0, self.obj_width)
+        self.a5.set_early_apply(False)
+        self.a5.set_custom_exec_cb(lambda a,v: self.set_width(self.obj3,v))
+        self.a5.set_path_cb(lv.anim_t.path_overshoot)
+        self.a5.set_time(300)
+        
+        self.a6 = lv.anim_t()
+        self.a6.init()
+        self.a6.set_values(0, self.obj_height)
+        self.a6.set_early_apply(False)
+        self.a6.set_custom_exec_cb(lambda a,v: self.set_height(self.obj3,v))
+        self.a6.set_path_cb(lv.anim_t.path_ease_out)
+        self.a6.set_time(300)
     
-    if obj == btn :
-        reverse = btn.has_state(lv.STATE.CHECKED)
-        lv.anim_timeline_set_reverse(anim_timeline,reverse)
-        lv.anim_timeline_start(anim_timeline)
-    elif obj == slider:
+        # Create anim timeline
+        print("Create new anim_timeline")
+        self.anim_timeline = lv.anim_timeline_create()
+        lv.anim_timeline_add(self.anim_timeline, 0, self.a1)
+        lv.anim_timeline_add(self.anim_timeline, 0, self.a2)
+        lv.anim_timeline_add(self.anim_timeline, 200, self.a3)
+        lv.anim_timeline_add(self.anim_timeline, 200, self.a4)
+        lv.anim_timeline_add(self.anim_timeline, 400, self.a5)
+        lv.anim_timeline_add(self.anim_timeline, 400, self.a6)
+        
+    def slider_prg_event_handler(self,e):
+        slider = e.get_target()
+
+        if  not self.anim_timeline:
+            self.anim_timeline_create()
+
         progress = slider.get_value()
-        lv.anim_timeline_set_progress(anim_timeline, progress)
+        lv.anim_timeline_set_progress(self.anim_timeline, progress)
 
 
-#
-# Create an animation timeline
-#
+    def btn_run_event_handler(self,e):
+        btn = e.get_target()
+        if not self.anim_timeline:
+            self.anim_timeline_create()
+        
+        reverse = btn.has_state(lv.STATE.CHECKED)
+        lv.anim_timeline_set_reverse(self.anim_timeline,reverse)
+        lv.anim_timeline_start(self.anim_timeline)
 
-par = lv.scr_act()
-par.set_flex_flow(lv.FLEX_FLOW.ROW)
-par.set_flex_align(lv.FLEX_ALIGN.SPACE_AROUND, lv.FLEX_ALIGN.CENTER, lv.FLEX_ALIGN.CENTER)
-
-btn = lv.btn(par)
-btn.add_event_cb(event_handler, lv.EVENT.VALUE_CHANGED, None)
-btn.add_flag(lv.obj.FLAG.IGNORE_LAYOUT)
-btn.add_flag(lv.obj.FLAG.CHECKABLE)
-btn.align(lv.ALIGN.BOTTOM_LEFT, 20, -20)
-
-slider = lv.slider(par)
-slider.add_event_cb(event_handler, lv.EVENT.VALUE_CHANGED, None)
-slider.add_flag(lv.obj.FLAG.IGNORE_LAYOUT)
-slider.align(lv.ALIGN.BOTTOM_RIGHT, -20, -20)
-slider.set_range(0, 65535)
-
-obj1 = lv.obj(par)
-obj1.set_size(obj_width, obj_height)
-
-obj2 = lv.obj(par)
-obj2.set_size(obj_width, obj_height)
-
-obj3 = lv.obj(par)
-obj3.set_size(obj_width, obj_height)
+    def btn_del_event_handler(self,e):
+        if self.anim_timeline:
+            lv.anim_timeline_del(self.anim_timeline)
+        self.anim_timeline = None;
 
 
-# obj1 
-a1 = lv.anim_t()
-a1.init()
-a1.set_values(0, obj_width)
-a1.set_early_apply(False)
-a1.set_custom_exec_cb(lambda a,v: set_width(obj1,v))
-a1.set_path_cb(lv.anim_t.path_overshoot)
-a1.set_time(300)
-#a1.user_data = obj1
-
-a2 = lv.anim_t()
-a2.init()
-a2.set_values(0, obj_height)
-a2.set_early_apply(False)
-a2.set_custom_exec_cb(lambda a,v: set_height(obj1,v))
-a2.set_path_cb(lv.anim_t.path_ease_out)
-a2.set_time(300)
-#a2.user_data = obj1
-
-# obj2 
-a3=lv.anim_t()
-a3.init()
-a3.set_values(0, obj_width)
-a3.set_early_apply(False)
-a3.set_custom_exec_cb(lambda a,v: set_width(obj2,v))
-a3.set_path_cb(lv.anim_t.path_overshoot)
-a3.set_time(300)
-#a3.user_data = obj2
-
-a4 = lv.anim_t()
-a4.init()
-a4.set_values(0, obj_height)
-a4.set_early_apply(False)
-a4.set_custom_exec_cb(lambda a,v: set_height(obj2,v))
-a4.set_path_cb(lv.anim_t.path_ease_out)
-a4.set_time(300)
-#a4.user_data = obj2
-
-# obj3 
-a5 = lv.anim_t()
-a5.init()
-a5.set_values(0, obj_width)
-a5.set_early_apply(False)
-a5.set_custom_exec_cb(lambda a,v: set_width(obj3,v))
-a5.set_path_cb(lv.anim_t.path_overshoot)
-a5.set_time(300)
-#a5.user_data = obj3
-
-a6 = lv.anim_t()
-a6.init()
-a6.set_values(0, obj_height)
-a6.set_early_apply(False)
-a6.set_custom_exec_cb(lambda a,v: set_height(obj3,v))
-a6.set_path_cb(lv.anim_t.path_ease_out)
-a6.set_time(300)
-#a6.user_data = obj3
-               
-# Create anim timeline 
-anim_timeline = lv.anim_timeline_create()
-lv.anim_timeline_add(anim_timeline, 0, a1)
-lv.anim_timeline_add(anim_timeline, 0, a2)
-lv.anim_timeline_add(anim_timeline, 200, a3)
-lv.anim_timeline_add(anim_timeline, 200, a4)
-lv.anim_timeline_add(anim_timeline, 400, a5)
-lv.anim_timeline_add(anim_timeline, 400, a6)
-
-
+lv_example_anim_timeline_1 = LV_ExampleAnimTimeline_1()
