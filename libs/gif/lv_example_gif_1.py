ea3c4cba924b84462034074dfe476d59dc11f6df
Uli Raich <uli.raich@gmail.com>
2021-11-01 11:07:00 +0100

    added libs examples

---
+++ libs/gif/lv_example_gif_1.py
@@ -0,0 +1,15 @@
+#!/opt/bin/lv_micropython -i
+import sys
+import lvgl as lv
+import display_driver
+import fs_driver
+
+fs_drv = lv.fs_drv_t()
+fs_driver.fs_register(fs_drv, 'S')
+
+img = lv.img(lv.scr_act())
+# The File system is attached to letter 'S'
+
+img.set_src("S:bulb.gif")
+img.align(lv.ALIGN.RIGHT_MID, -20, 0)
+img.center()
