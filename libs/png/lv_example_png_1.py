348d90dec301eb1d5b749c724bae11bc8d7cec41
Uli Raich <uli.raich@gmail.com>
2021-11-04 16:12:14 +0100

    completed png example

--- libs/png/lv_example_png_1.py
+++ libs/png/lv_example_png_1.py
@@ -3,11 +3,22 @@ import sys
 import lvgl as lv
 import display_driver
 from imagetools import get_png_info, open_png
-
+from img_wink_png import img_wink_png_map
 # Register PNG image decoder
 decoder = lv.img.decoder_create()
 decoder.info_cb = get_png_info
 decoder.open_cb = open_png
+
+img_wink_png =  lv.img_dsc_t(
+    {
+        "header": {"always_zero": 0, "w": 50, "h": 50,  "cf": lv.img.CF.RAW_ALPHA},
+        "data_size": 5158,
+        "data": img_wink_png_map,
+    }
+)
+img1 = lv.img(lv.scr_act())
+img1.set_src(img_wink_png)
+img1.align(lv.ALIGN.RIGHT_MID, -250, 0)
 
 # Create an image from the png file
 try:
@@ -22,6 +33,6 @@ wink_argb = lv.img_dsc_t({
   'data': png_data 
 })
 
-img = lv.img(lv.scr_act())
-img.set_src(wink_argb)
-img.center()
+img2 = lv.img(lv.scr_act())
+img2.set_src(wink_argb)
+img2.align(lv.ALIGN.RIGHT_MID, -150, 0)
